380d46f9b7b5cc566aefa23c938823e299fb69d4
Bella Sinclair <bsm.sinclair@gmail.com>
2019-04-11 13:23:40 +0200

    corrected small errors to the sbatch script

--- code/assembly/canu_assembly.sh
+++ code/assembly/canu_assembly.sh
@@ -9,11 +9,13 @@
 
 # Load modules
 module load bioinfo-tools
-module load Canu/1.7
+module load canu/1.7
 
-# Assembly with canu
+# Assembly with canu. I set the genome size to 2.4 mbp because that is the size of
+# the draft genome for L. ferriphilum which feels lika a good estimate if I did not
+# know the results of the paper. 
 canu \
 -p 1_canu_dna_assembly -d /home/bella/genome_analysis/dna_analysis/assembly \
 genomeSize=2.4m \
 stopOnReadQuality=false \
--pacbio-raw /home/bella/genome_analysis/data/dna/ERR2028?.fastq.gz
+-pacbio-raw /home/bella/genome_analysis/data/dna/*.fastq.gz
