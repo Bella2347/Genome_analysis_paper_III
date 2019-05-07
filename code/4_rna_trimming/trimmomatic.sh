725185efccd4e72518503a542d5d7abbcc1aa334
Bella Sinclair <bsm.sinclair@gmail.com>
2019-05-07 15:37:50 +0200

    trying to figure out why it fails

--- code/4_rna_trimming/trimmomatic.sh
+++ code/4_rna_trimming/trimmomatic.sh
@@ -12,8 +12,7 @@ module load bioinfo-tools
 module load trimmomatic/0.36
 
 java -jar $TRIMMOMATIC_HOME/trimmomatic.jar PE -phred33 \
-/home/bella/genome_analysis/data/rna/batch_14B_31_1.fastq.gz \ 
-/home/bella/genome_analysis/data/rna/batch_14B_31_2.fastq.gz \
+/home/bella/genome_analysis/data/rna/batch_14B_31_1.fastq.gz /home/bella/genome_analysis/data/rna/batch_14B_31_2.fastq.gz \
 trimmed_batch_14B_31_1_paired.fastq.gz trimmed_batch_14B_31_1_unpaired.fastq.gz \
 trimmed_batch_14B_31_2_paired.fastq.gz trimmed_batch_14B_31_2_unpaired.fastq.gz \
 ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10 LEADING:28 TRAILING:28 MINLEN:36
