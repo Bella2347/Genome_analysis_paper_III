e6de9eb4559048d2841751b2993c6e41db4ece8e
Bella Sinclair <bsm.sinclair@gmail.com>
2019-05-10 17:03:20 +0200

    added line for indexing and the mapping for the batch files

--- code/5_rna_mapping/bwa.sh
+++ code/5_rna_mapping/bwa.sh
@@ -10,3 +10,18 @@
 # Load modules
 module load bioinfo-tools bwa/0.7.17
 
+# Make index of the assembly
+# bwa index ../../dna_analysis/1_assembly/1_canu_dna_assembly.contigs.fasta
+
+bwa mem ../../dna_analysis/1_assembly/1_canu_dna_assembly.contigs.fasta ../../data/rna/trimmed_reads/trimmed_batch_14B_31_P1.fastq.gz \ 
+../../data/rna/trimmed_reads/trimmed_batch_14B_31_P2.fastq.gz > bwa_b1.sam
+
+bwa mem ../../dna_analysis/1_assembly/1_canu_dna_assembly.contigs.fasta ../../data/rna/trimmed_reads/trimmed_batch_14B_90_P1.fastq.gz \
+../../data/rna/trimmed_reads/trimmed_batch_14B_90_P2.fastq.gz > bwa_b2.sam
+
+bwa mem ../../dna_analysis/1_assembly/1_canu_dna_assembly.contigs.fasta ../../data/rna/trimmed_reads/trimmed_batch_14C_32_P1.fastq.gz \
+../../data/rna/trimmed_reads/trimmed_batch_14C_32_P2.fastq.gz > bwa_b3.sam
+
+bwa mem ../../dna_analysis/1_assembly/1_canu_dna_assembly.contigs.fasta ../../data/rna/trimmed_reads/trimmed_batch_14C_91_P1.fastq.gz \
+../../data/rna/trimmed_reads/trimmed_batch_14C_91_P2.fastq.gz > bwa_b4.sam
+
